59e121eba8859bdae68e20cb62916e7461795569
zuulaxx <guigui.8929@gmail.com>
2023-03-23 16:58:18 +0100

    add characters

--- game/Player.lua
+++ game/Player.lua
@@ -1,8 +1,22 @@
 local Player = BaseObject:new()
 
-function Player:new()
-    self.pushRadius = 0
+function Player:new(name)
+    self.name = name
+    self.widthRadius = 9
+    self.heightRadius = 19
+    self.pushRadius = 10
     self.slopeFactor = 0
+    self.jumpForce = 6.5
+    self.gravityForce = 0.21875
+
+    if name == "sonic" then
+    elseif name == "tails" then
+        -- standing
+        self.widthRadius = 9
+        self.heightRadius = 15
+    elseif name == "knuckles" then
+        self.jumpForce = 6
+    end
 end
 
 return Player
